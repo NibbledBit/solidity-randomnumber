1a966e4083f51957551d921e9bb3aafecf0f2a3b
Nibbles <me@nibbledbit.co.uk>
2022-03-14 14:19:53 +0000

    added psuedo RNG contract

---
+++ contracts/PsuedoRandomNumber.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// An example of a consumer contract that relies on a subscription for funding.
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract PsuedoRandomNumber is VRFConsumerBaseV2 {
+    function requestRandomNumber() public returns (uint256) {}
+}
